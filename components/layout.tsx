a3abbd03773329bdfb34aff0d7cae494eaf1a936
masoudNemati <masoud.nnr@gmail.com>
2020-09-09 19:52:26 +0430

    just crated empty pages and try to build navbar

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,4 @@
-import Link from "next/link";
-
+import Navbar from "./nav/Navbar";
 export interface ChildrenProps {
   children: any;
 }
@@ -7,23 +6,7 @@ export interface ChildrenProps {
 function Layout({ children }: ChildrenProps) {
   return (
     <div>
-      <header>
-        <Link href="/">
-          <a>صفحه اصلی</a>
-        </Link>
-        <Link href="/songs">
-          <a>آهنگ</a>
-        </Link>
-        <Link href="/music-video">
-          <a>موزیک ویدئو</a>
-        </Link>
-        <Link href="/about-us">
-          <a>درباره ما</a>
-        </Link>
-        <Link href="/contact-us">
-          <a>تماس با ما</a>
-        </Link>
-      </header>
+      <Navbar />
       {children}
       <footer>&copy; {new Date().getFullYear()}</footer>
     </div>
---
+++ pages/about-us.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AboutUs = () => {
+  return <div>About Us page</div>;
+};
+
+export default AboutUs;
