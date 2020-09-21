b7cf8a00b5fe96c61bdf431c76b78527100ff42b
masoudnemati <masoud.nnr@gmail.com>
2020-09-21 15:47:14 +0330

    created Hero component and added audiwide and vazir font to document

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,25 +7,33 @@ const Hero = () => {
         <div className="headers">
           <h1 className="hero-header">EMIN RASEN</h1>
           <h4 className="hero-des">TURKMEN RAP İÑ DALISI</h4>
-          <div className="icons">
-            <div className="instagram-icon">
-              <a
-                href="https://www.instagram.com/amin_rasen/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Instagram
-              </a>
-            </div>
-            <div>
-              <a
-                href="https://t.me/rasenrap1"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Telegram
-              </a>
-            </div>
+          <div className="instagram-icon">
+            <a
+              href="https://www.instagram.com/amin_rasen/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="/icons/icons8-instagram-old-50.png"
+                alt="emin rasen instagram"
+                width="35px"
+                height="35px"
+              />
+            </a>
+          </div>
+          <div className="telegram-icon">
+            <a
+              href="https://t.me/rasenrap1"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="/icons/icons8-telegram-app-50.png"
+                alt="emin rasen instagram"
+                width="35px"
+                height="35px"
+              />
+            </a>
           </div>
         </div>
       </div>
@@ -41,8 +49,10 @@ const Hero = () => {
 
         .headers {
           background-color: #021c3cee;
+          height: 200px;
         }
         .hero-header {
+          font-family: "Audiowide", cursive;
           margin: 0;
           padding: 0;
           padding-top: 20px;
@@ -52,9 +62,18 @@ const Hero = () => {
         .hero-des {
           margin: 0;
           padding-top: 8px;
-          padding-bottom: 18px;
+          padding-bottom: 30px;
 
           color: rgb(187, 187, 187);
+        }
+
+        .instagram-icon {
+          display: inline;
+        }
+
+        .telegram-icon {
+          display: inline;
+          margin-left: 10px;
         }
       `}</style>
     </main>
