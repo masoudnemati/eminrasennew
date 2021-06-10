9a140453e5b15c8031061e4c22965148f883ad1b
masoudnemati <masoud.nnr@gmail.com>
2021-06-10 10:41:11 +0430

    just improved social media links in hero section and trying to deploy to liara

--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -7,41 +7,43 @@ const Hero = () => {
         <div className="headers">
           <h1 className="hero-header">EMIN RASEN</h1>
           <h4 className="hero-des">TURKMEN RAP İÑ DALISI</h4>
-          <div className="instagram-icon">
-            <a
-              href="https://www.instagram.com/amin_rasen/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="/icons/icons8-instagram-old-50.png"
-                alt="emin rasen instagram"
-                width="35px"
-                height="35px"
-              />
-            </a>
-          </div>
-          <div className="telegram-icon">
-            <a
-              href="https://t.me/rasenrap1"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="/icons/icons8-telegram-app-50.png"
-                alt="emin rasen instagram"
-                width="35px"
-                height="35px"
-              />
-            </a>
-          </div>
+          <section className="social-icons">
+            <div>
+              <a
+                href="https://www.instagram.com/amin_rasen/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="/icons/icons8-instagram-old-50.png"
+                  alt="emin rasen instagram"
+                  width="35px"
+                  height="35px"
+                />
+              </a>
+            </div>
+            <div>
+              <a
+                href="https://t.me/rasenrap1"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="/icons/icons8-telegram-app-50.png"
+                  alt="emin rasen instagram"
+                  width="35px"
+                  height="35px"
+                />
+              </a>
+            </div>
+          </section>
         </div>
       </div>
 
       <style jsx>{`
         .hero {
           text-align: center;
-          padding: 150px 0 300px 0;
+          padding: 150px 0 280px 0;
           background-image: url("/images/eminrasenhero.webp");
           background-size: cover;
           background-position: center;
@@ -49,7 +51,7 @@ const Hero = () => {
 
         .headers {
           background-color: #021c3cee;
-          height: 200px;
+          height: 210px;
         }
         .hero-header {
           font-family: "Audiowide", cursive;
@@ -70,13 +72,26 @@ const Hero = () => {
           color: rgb(187, 187, 187);
         }
 
-        .instagram-icon {
-          display: inline;
+        .social-icons {
+          display: flex;
+          align-items: center;
+          justify-content: center;
         }
 
-        .telegram-icon {
-          display: inline;
-          margin-left: 10px;
+        img {
+          background-color: #0c1422;
+          height: 35px;
+          width: 35px;
+          margin: ‌ 100px;
+          box-sizing: content-box;
+          padding: 15px;
+          margin: 0 10px;
+          border-radius: 40%;
+          transition: all 0.2s ease;
+        }
+
+        img:hover {
+          box-shadow: 0 3px 3px #8fbfcf;
         }
       `}</style>
     </main>
